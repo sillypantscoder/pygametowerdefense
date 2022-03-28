b08a50354f4d514124f18c0b2c03908dd47cd867
Jason Roman <funjasonroman@gmail.com>
2022-03-28 09:47:25 -0600

    Add entities, follow path

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+import math
 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
@@ -11,6 +12,38 @@ CELLSIZE = 50
 
 BOARD = [[random.choices([0, 1], weights=[10, 1], k=1)[0] for y in range(10)] for x in range(10)]
 ROUTE = [[0, 3], [5, 3], [5, 10]]
+
+def routecopy(r: "list[list[int]]" = ROUTE) -> "list[list[int]]":
+	e = []
+	for i in r: e.append(i.copy())
+	return e
+
+class Entity:
+	speed = 0.1
+	def __init__(self, route: "list[list[int]]" = ROUTE):
+		self.route = routecopy(route)
+		self.pos = self.route[0].copy()
+		self.prevpos = self.route.pop(0)
+		self.ticks = 0
+	def draw(self):
+		r = pygame.Surface((10, 10))
+		r.fill((0, 0, 0))
+		return r
+	def tick(self):
+		dx = self.route[0][0] - self.prevpos[0]
+		dy = self.route[0][1] - self.prevpos[1]
+		numticks = math.sqrt(dx + dy) / self.speed
+		if self.ticks + 1 < numticks:
+			self.ticks += 1
+			self.pos[0] += dx / numticks
+			self.pos[1] += dy / numticks
+		else:
+			self.prevpos = self.route.pop(0)
+			self.ticks = 0
+		if len(self.route) == 0: exit()
+
+e = Entity()
+cellno_to_pixel = (lambda x: round((x * CELLSIZE) + (0.5 * CELLSIZE)))
 
 c = pygame.time.Clock()
 running = True
@@ -33,10 +66,16 @@ while running:
 				pygame.draw.rect(screen, BLACK, cellrect)
 	# Route
 	for i in range(len(ROUTE) - 1):
-		pixeltransform = (lambda x: round((x * CELLSIZE) + (0.5 * CELLSIZE)))
-		start = (pixeltransform(ROUTE[i][1]), pixeltransform(ROUTE[i][0]))
-		end = (pixeltransform(ROUTE[i + 1][1]), pixeltransform(ROUTE[i + 1][0]))
-		pygame.draw.line(screen, RED, start, end, round(0.2 * CELLSIZE))
+		start = (cellno_to_pixel(ROUTE[i][0]), cellno_to_pixel(ROUTE[i][1]))
+		end = (cellno_to_pixel(ROUTE[i + 1][0]), cellno_to_pixel(ROUTE[i + 1][1]))
+		pygame.draw.line(screen, RED, start, end, round(0.3 * CELLSIZE))
+	# Entities
+	e.tick()
+	s = e.draw()
+	x = cellno_to_pixel(e.pos[0])# - (s.get_width() / 2)
+	y = cellno_to_pixel(e.pos[1]) - (s.get_height() / 2)
+	screen.blit(s, (x, y))
+	#pygame.draw.line(screen, BLACK, [cellno_to_pixel(e.prevpos[0]), cellno_to_pixel(e.prevpos[1])], [cellno_to_pixel(e.route[0][0]), cellno_to_pixel(e.route[0][1])], 1)
 	# Flip
 	pygame.display.flip()
-	c.tick(60)
+	c.tick(20)
