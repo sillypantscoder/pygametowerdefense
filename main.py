2b4a31d2570469a4a7e5b710e335b402a2c1770d
Jason Roman <funjasonroman@gmail.com>
2022-03-28 08:39:47 -0600

    Pygame basic starter program

---
+++ main.py
@@ -0,0 +1,18 @@
+import pygame
+
+WHITE = (255, 255, 255)
+
+SCREENSIZE = [500, 500]
+screen = pygame.display.set_mode(SCREENSIZE)
+
+c = pygame.time.Clock()
+running = True
+while running:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			running = False
+	# Drawing
+	screen.fill(WHITE)
+	# Flip
+	pygame.display.flip()
+	c.tick(60)
